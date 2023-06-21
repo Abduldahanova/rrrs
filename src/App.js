e827ae54854f4bbee7ebd45056f5daad70319ce5
EmirSoltonbek <emirsoltonbekov7@gmail.com>
2023-06-21 17:38:29 +0600

    mu

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">lllluuuu</div>;
+  return (
+    <div className="App">
+      lllluuuu
+      <div>muuuu</div>
+    </div>
+  );
 }
 
 export default App;
